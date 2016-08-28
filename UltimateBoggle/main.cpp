297bf98f9cb6b9ee27b9edeceb9ccfc5575f333a
Aleksandr Shevchenko <s.aleksandras@gmail.com>
2016-08-29 01:29:05 +0300

    Reworking it

--- UltimateBoggle/main.cpp
+++ UltimateBoggle/main.cpp
@@ -12,15 +12,19 @@
 
 int main () try {
     using namespace ultimate_boggle;    
-    dictionary s_dictionary ("dict.txt");
-    
+    dictionary s_dictionary ("dict.txt");    
     board s_board ("board.txt");
     solver_dfs s_solver (s_dictionary);
     std::vector<const char*> s_wordlist;    
-    TIMEIT (s_solver.solve (s_board, s_wordlist));
-    for (const auto& s_word : s_wordlist) {
-        std::cout << s_word << "\n";
+    //TIMEIT (
+    for (auto i = 0; i < 20; ++i) {
+        s_solver.solve (s_board, s_wordlist);
+        s_wordlist.clear ();
     }
+    //);
+    //for (const auto& s_word : s_wordlist) {
+    //    std::cout << s_word << "\n";
+    //}
     
     return 0;
 }
